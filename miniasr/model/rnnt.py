326c8675b46f32edf079964a7ed3c2cc0da723d6
melody <b08504045@ntu.edu.tw>
2022-09-24 03:00:05 +0800

    rnnt-try

--- miniasr/model/rnnt.py
+++ miniasr/model/rnnt.py
@@ -129,3 +129,122 @@ class DecoderRNNT(nn.Module):
 
         return outputs, hidden_states
     
+    
+class RNNTransducer(nn.Module):
+    """
+    RNN-Transducer are a form of sequence-to-sequence models that do not employ attention mechanisms.
+    Unlike most sequence-to-sequence models, which typically need to process the entire input sequence
+    (the waveform in our case) to produce an output (the sentence), the RNN-T continuously processes input samples and
+    streams output symbols, a property that is welcome for speech dictation. In our implementation,
+    the output symbols are the characters of the alphabet.
+    Args:
+        num_classes (int): number of classification
+        input_dim (int): dimension of input vector
+        num_encoder_layers (int, optional): number of encoder layers (default: 4)
+        num_decoder_layers (int, optional): number of decoder layers (default: 1)
+        encoder_hidden_state_dim (int, optional): hidden state dimension of encoder (default: 320)
+        decoder_hidden_state_dim (int, optional): hidden state dimension of decoder (default: 512)
+        output_dim (int, optional): output dimension of encoder and decoder (default: 512)
+        rnn_type (str, optional): type of rnn cell (default: lstm)
+        bidirectional (bool, optional): if True, becomes a bidirectional encoder (default: True)
+        encoder_dropout_p (float, optional): dropout probability of encoder
+        decoder_dropout_p (float, optional): dropout probability of decoder
+        sos_id (int, optional): start of sentence identification
+        eos_id (int, optional): end of sentence identification
+    Inputs: inputs, input_lengths, targets, target_lengths
+        inputs (torch.FloatTensor): A input sequence passed to encoder. Typically for inputs this will be a padded
+            `FloatTensor` of size ``(batch, seq_length, dimension)``.
+        input_lengths (torch.LongTensor): The length of input tensor. ``(batch)``
+        targets (torch.LongTensr): A target sequence passed to decoder. `IntTensor` of size ``(batch, seq_length)``
+        target_lengths (torch.LongTensor): The length of target tensor. ``(batch)``
+    Returns:
+        * predictions (torch.FloatTensor): Result of model predictions.
+    """
+    def __init__(
+            self,
+            num_classes: int,
+            input_dim: int,
+            num_encoder_layers: int = 4,
+            num_decoder_layers: int = 1,
+            encoder_hidden_state_dim: int = 320,
+            decoder_hidden_state_dim: int = 512,
+            output_dim: int = 512,
+            rnn_type: str = "lstm",
+            bidirectional: bool = True,
+            encoder_dropout_p: float = 0.2,
+            decoder_dropout_p: float = 0.2,
+            sos_id: int = 1,
+            eos_id: int = 2,
+    ):
+        super(RNNTransducer, self).__init__()
+        self.num_classes = num_classes
+        self.encoder = EncoderRNNT(
+            input_dim=input_dim,
+            hidden_state_dim=encoder_hidden_state_dim,
+            output_dim=output_dim,
+            num_layers=num_encoder_layers,
+            rnn_type=rnn_type,
+            dropout_p=encoder_dropout_p,
+            bidirectional=bidirectional,
+        )
+        self.decoder = DecoderRNNT(
+            num_classes=num_classes,
+            hidden_state_dim=decoder_hidden_state_dim,
+            output_dim=output_dim,
+            num_layers=num_decoder_layers,
+            rnn_type=rnn_type,
+            sos_id=sos_id,
+            eos_id=eos_id,
+            dropout_p=decoder_dropout_p,
+        )
+        self.fc = Linear(decoder_hidden_state_dim, num_classes, bias=False)
+
+    def set_encoder(self, encoder):
+        """ Setter for encoder """
+        self.encoder = encoder
+
+    def set_decoder(self, decoder):
+        """ Setter for decoder """
+        self.decoder = decoder
+
+    def count_parameters(self) -> int:
+        """ Count parameters of model """
+        num_encoder_parameters = self.encoder.count_parameters()
+        num_decoder_parameters = self.decoder.count_parameters()
+        return num_encoder_parameters + num_decoder_parameters
+
+    def update_dropout(self, dropout_p) -> None:
+        """ Update dropout probability of model """
+        self.encoder.update_dropout(dropout_p)
+        self.decoder.update_dropout(dropout_p)
+        
+    def joint(self, encoder_outputs: Tensor, decoder_outputs: Tensor) -> Tensor:
+        
+        if encoder_outputs.dim() == 3 and decoder_outputs.dim() == 3:
+            input_length = encoder_outputs.size(1)
+            target_length = decoder_outputs.size(1)
+
+            encoder_outputs = encoder_outputs.unsqueeze(2)
+            decoder_outputs = decoder_outputs.unsqueeze(1)
+
+            encoder_outputs = encoder_outputs.repeat([1, 1, target_length, 1])
+            decoder_outputs = decoder_outputs.repeat([1, input_length, 1, 1])
+
+        outputs = torch.cat((encoder_outputs, decoder_outputs), dim=-1)
+        outputs = self.fc(outputs)
+
+        return outputs
+
+    def forward(
+            self,
+            inputs: Tensor,
+            input_lengths: Tensor,
+            targets: Tensor,
+            target_lengths: Tensor
+    ) -> Tensor:
+        encoder_outputs, _ = self.encoder(inputs, input_lengths)
+        decoder_outputs, _ = self.decoder(targets, target_lengths)
+        outputs = self.joint(encoder_outputs, decoder_outputs)
+        return outputs
+
+    
