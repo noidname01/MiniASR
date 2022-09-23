b58feb4f5bcc887fa6eaf119cf35d0679049b6b4
ba144220 <hs41511@gmail.com>
2022-09-24 05:27:38 +0800

    fix

--- miniasr/model/ctc_asr.py
+++ miniasr/model/ctc_asr.py
@@ -20,7 +20,7 @@ from miniasr.module import RNNEncoder
 # import pickle
 # import numpy as np
 from miniasr.prefix_beam_search import prefix_beam_search
-from miniasr.language_model import LanguageModel
+from miniasr.prefix_beam_search import language_model
 
 
 
