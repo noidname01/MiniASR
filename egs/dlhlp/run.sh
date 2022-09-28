6ee90e625596b9b71b6caf025703efc56034a12f
ba144220 <hs41511@gmail.com>
2022-09-28 14:24:39 +0800

    update

--- egs/dlhlp/run.sh
+++ egs/dlhlp/run.sh
@@ -5,7 +5,7 @@
 stage=3
 stop_stage=3
 model_name=ctc_libri-10h_char
-ckpt=model/ctc_libri-10h_char/epoch=26-step=4643.ckpt
+ckpt=model/ctc_libri-10h_char/epoch=29-step=5159.ckpt
 
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
